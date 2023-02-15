b822531055972a8305949a4379801a253ded4090
CHAPUT Fréderic <FChaput@arfp.asso.fr>
2023-02-15 12:25:20 +0100

    Suite fourchette 15/2 midi

--- Java/Ob03_Fouchette_retour/src/Ob03_Fouchette_retour/App.java
+++ Java/Ob03_Fouchette_retour/src/Ob03_Fouchette_retour/App.java
@@ -1,10 +1,184 @@
 package Ob03_Fouchette_retour;
-
+import java.util.*;
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Fourchette fork = new Fourchette();
+		//------
+		
+		Boolean rejouer = fork.setJouerEncore(true); 
+		String encore = "r";
+		String again; 
+		String againCasse;
+		//-----
+		
+		
+		Boolean jouerContreOrdi = fork.setJouerContrePC(true);
+		String pc ="pc";
+		String contrepc;
+		String contrepcCase;
+		//----
+		
+		Boolean joueurGagne = fork.victoireUser();
+		Boolean pcGagne = fork.victoireComputer();
+		Boolean egal = fork.egalite();
+		Boolean perdu = fork.joueurPerdSeul();
+		//------
+		
+		int proposition = fork.propositionUser(0);
+		Boolean valide = fork.validerPropositionUser();
+		
+		int max = fork.getMaxUser();
+		int min = fork.getMinUser(); 
+		
+		
+		
+		do {
+			System.out.println("Saisissez PC si vous souhaitez jouer contre l'ordinateur sinon saisissez n'importe quelle lettre.");
+			contrepc = sc.nextLine();
+			contrepcCase =contrepc.toLowerCase();
+			
+			if (contrepcCase.equals(pc)) {
+				jouerContreOrdi = true;
+				fork.definirNombreAleatoire();
+				fork.jouerContrePC();
+				do {
+					
+					do {
+						do {
+							System.out.println("saisisez un nouveau nombre entre "+min+" et "+max);
+							proposition = sc.nextInt();
+							
+							valide = fork.validerPropositionUser();
+							
+							if (valide == false) {  // ???????????????????????????????????????????????
+								System.out.println("Saisie invalide ! Veuillez recommencer.");
 
+							}
+							
+							else {
+								valide = true;
+								
+							}
+							
+							
+						} while (valide == false);
+						
+						fork.nouvelleFourcehtteUser();
+						joueurGagne = fork.victoireUser();
+						egal = fork.egalite();
+						fork.definirPropositionComputer();
+						System.out.println("L'ordinateur a joué.");
+						pcGagne = fork.victoireComputer();
+						egal = fork.egalite();
+						
+						
+						
+					} while (joueurGagne == false && pcGagne == false && egal == false ); 
+					
+					
+					
+					
+					jouerContreOrdi = false;
+					
+					
+					
+				}				
+				while (jouerContreOrdi == true) ;
+				
+			}
+			//--------------------------------------------------------------------------
+			else {
+				jouerContreOrdi = false;
+				
+				do {
+				do {
+					do {
+						System.out.println("saisisez un nouveau nombre entre "+min+" et "+max);
+						proposition = sc.nextInt();
+						
+						valide = fork.validerPropositionUser();
+						
+						if (valide == false) {  // ???????????????????????????????????????????????
+							System.out.println("Saisie invalide ! Veuillez recommencer.");
+
+						}
+						
+						else {
+							valide = true;
+							
+						}
+						
+						
+					} while (valide == false);
+					
+					fork.nouvelleFourcehtteUser();
+					joueurGagne = fork.victoireUser();
+					perdu = fork.joueurPerdSeul(); 
+					
+					
+					
+				} while (joueurGagne == false && perdu == false  ); 
+				}
+				
+				while (jouerContreOrdi == false) ;
+			}
+			
+			
+			
+			
+			
+			
+				
+				
+				
+				
+				
+			
+			
+			
+			
+			System.out.println("Appuyez sur R jour rejouer ou sur une autre lettre pour quitter la partie");
+			again = sc.nextLine();
+			againCasse = again.toLowerCase();
+			
+			if (againCasse.equals(encore)) {
+				rejouer = true;
+			}
+			
+			else {
+				rejouer = false;
+			}
+		} while (rejouer == true) ; 
+		
+		
+		System.out.println("Merci d'avoir jouer et à bientot");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- Java/Ob03_Fouchette_retour/src/Ob03_Fouchette_retour/Fourchette.java
+++ Java/Ob03_Fouchette_retour/src/Ob03_Fouchette_retour/Fourchette.java
@@ -13,7 +13,7 @@ public class Fourchette {
 	private int minUser;
 	private int maxUser;
 	private int minComputer;
-	private int maxComputeur;
+	private int maxComputer;
 	private int nombreAtrouver;
 	private int propositionUser;
 	private int propositionComputer;
@@ -22,8 +22,7 @@ public class Fourchette {
 	private int nombreEssaisMax;
 	private Boolean rejoueEncore;
 	private Boolean jpueContrePC;
-	private Boolean gagneUser;
-	private Boolean gagneComputer;
+
 	// Les constructeurs
 	// --------------------------------------------------------------------
 
@@ -31,19 +30,58 @@ public class Fourchette {
 		this.min = 0;
 		this.max = 100;
 		this.nombreEssaisMax = 10;
-		this.jpueContrePC = true;
+
 	}
 
-	public Fourchette(int _min, int _max, int _nombreEssaisMax, Boolean _jpueContrePC) {
+	public Fourchette(int _min, int _max, int _nombreEssaisMax) {
 		this.min = _min;
 		this.max = _max;
 		this.nombreEssaisMax = _nombreEssaisMax;
-		this.jpueContrePC = _jpueContrePC;
+
 	}
 
-	// get
+	// set
 	// ---------------------------------------------------------------------------------
 
+	public boolean setJouerEncore(Boolean _rejoueEncore) {
+		this.rejoueEncore = _rejoueEncore;
+
+		if (_rejoueEncore == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean setJouerContrePC(Boolean _jpueContrePC) {
+		this.jpueContrePC = _jpueContrePC;
+
+		if (_jpueContrePC == true) {
+			return this.jpueContrePC;
+		} else {
+			return this.jpueContrePC;}
+
+	}
+	
+	public int propositionUser(int _propositionUser ) {
+		this.propositionUser = _propositionUser;
+		return this.propositionUser;		
+	}
+	
+	// get -------------------------------------------------------------------------
+	
+	public int getMinUser() {
+		return this.minUser;
+	}
+
+	public int getMaxUser() {
+		return this.maxUser;
+	}
+	
+	public int getEssaisMax() {
+		return this.nombreEssaisMax;
+	}
+	
 	// les fonctions
 	// -----------------------------------------------------------------------------
 
@@ -53,21 +91,12 @@ public class Fourchette {
 	}
 
 	public int definirPropositionComputer() {
-		this.propositionComputer = r.nextInt(this.min, this.max);
+		this.propositionComputer = r.nextInt(this.minComputer, this.maxComputer);
 		return this.propositionComputer;
 	}
 
 	public boolean validerPropositionUser() {
-		if (this.propositionUser >= this.min && this.propositionUser <= this.max) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public boolean JouerEncore() {
-
-		if (this.rejoueEncore == true) {
+		if (this.propositionUser >= this.minUser && this.propositionUser <= this.maxUser) {
 			return true;
 		} else {
 			return false;
@@ -75,84 +104,116 @@ public class Fourchette {
 	}
 
 	public boolean jouerContrePC() {
-		
+
 		if (this.jpueContrePC == true) {
 			this.minUser = this.min;
 			this.maxUser = this.max;
-			this.minComputer = this.min; 
-			this.maxComputeur = this.max;
+			this.minComputer = this.min;
+			this.maxComputer = this.max;
 			return true;
-		}
-		else {
+		} else {
 			this.minUser = this.min;
 			this.maxUser = this.max;
-			return false;			
+			return false;
 		}
-		
+
 	}
- 	
+
 	public int nouvelleFourcehtteUser() {
-		
+
 		if (this.propositionUser < this.nombreAtrouver) {
 			this.minUser = this.propositionUser;
-			return this.minUser;			
+			return this.minUser;
 		}
-		
+
 		else if (this.propositionUser > this.nombreAtrouver) {
 			this.maxUser = this.propositionUser;
 			return this.maxUser;
-		}
-		else {
-			this.gagneUser = true;
+		} else {
+
 			return this.propositionUser;
 		}
-		
+
 	}
-	
-	
+
 	public int nouvelleFourcehttePC() {
-		
+
 		if (this.propositionComputer < this.nombreAtrouver) {
 			this.minComputer = this.propositionComputer;
-			return this.minComputer;			
+			return this.minComputer;
 		}
-		
+
 		else if (this.propositionComputer > this.nombreAtrouver) {
-			this.maxComputeur = this.propositionComputer;
-			return this.maxComputeur;
-		}
-		else {
-		
+			this.maxComputer = this.propositionComputer;
+			return this.maxComputer;
+		} else {
+
 			return this.propositionComputer;
 		}
-		
+
 	}
-	
-	
+
 	public boolean victoireUser() {
-		
-		if (this.jpueContrePC == true)
-			if ( this.nombreEssaisUser <= this.nombreEssaisMax ) {
-				this.gagneComputer = false;
-				
-				
+
+		if (this.jpueContrePC == true) {
+			if (this.nombreEssaisUser <= this.nombreEssaisMax && this.propositionUser == this.nombreAtrouver) {
+
+				System.out.println("Bravo ! Vous avez gagné !");
+
 			}
+			return true;
+		} else if (this.jpueContrePC == false) {
+			if (this.nombreEssaisUser <= this.nombreEssaisMax && this.propositionUser == this.nombreAtrouver) {
+
+				System.out.println("Bravo ! Vous avez gagné !");
+			}
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public boolean victoireComputer() {
+
+		if (this.jpueContrePC == true) {
+			if (this.nombreEssaisComputer <= this.nombreEssaisMax && this.propositionComputer == this.nombreAtrouver) {
+
+				System.out.println("L'ordinateur a gagné. Le nombre à trouver étéit : " + this.nombreAtrouver);
+
+			}
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public boolean egalite() {
+
+		if (this.jpueContrePC == true) {
+			if (this.nombreEssaisComputer == this.nombreEssaisMax && this.propositionComputer != this.nombreAtrouver
+					&& this.nombreEssaisUser == this.nombreEssaisMax && this.propositionUser != this.nombreAtrouver) {
+
+				System.out.println("Egalité !");
+			}
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public boolean joueurPerdSeul() {
 		
-		
-		
-	} // fin jouecontrepc true
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		if (this.jpueContrePC == false) {
+			if ( this.nombreEssaisUser == this.nombreEssaisMax && this.propositionUser != this.nombreAtrouver) {
+				System.out.println("Vous avez perdu ! Le nombre à trouver étéit : " + this.nombreAtrouver);
+			}
+			return true;
+		}
+		else {
+			return false; 
+		}
+	}
 }
